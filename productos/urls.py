b1195fc29b77bd1cad65366dcfe044fb36b30ff1
Sebastián Rivera <sbrivera2@espe.edu.ec>
2024-06-01 21:04:19 -0500

    Taller grupal

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URLs de vistas normales
-    # Crear la URL de la vista index
+    path('', views.listar_productos, name='listar_productos'),
+    path('crear/', views.crear_producto, name='crear_producto'),
+    path('editar/<int:id>/', views.editar_producto, name='editar_producto'),
+    path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
 
-productos = []
-
 def listar_productos(request):
-    # Consulta a la base de datos
-    # Renderiza la plantilla listar.html
+    productos = Producto.objects.all()
+    return render(request, 'listar.html', {'productos': productos})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+        Producto.objects.create(nombre=nombre, precio=precio, cantidad=cantidad)
+        return redirect('listar_productos')
+    return render(request, 'crear.html')
+
+def editar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    if request.method == 'POST':
+        producto.nombre = request.POST.get('nombre')
+        producto.precio = request.POST.get('precio')
+        producto.cantidad = request.POST.get('cantidad')
+        producto.save()
+        return redirect('listar_productos')
+    return render(request, 'editar.html', {'producto': producto})
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'eliminar.html', {'producto': producto})
